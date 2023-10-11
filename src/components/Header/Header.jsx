2d74c1028515c144a1c878a76e7320b5bbe76d7a
Delgorithm <delgorithm@gmail.com>
2023-10-11 20:28:33 +0200

    update and initialisation

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <p className='text-3xl bg-red-400'>azdiojaziozaj azda</p>
+    </div>
+  )
+}
+
+export default Header
