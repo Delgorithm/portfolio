09b53ce3ef51e78590d419caa72dae669578ea76
Delgorithm <delgorithm@gmail.com>
2024-05-12 19:20:30 +0200

    Fix : About to fix and refix the fixing fixed before fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-import React from 'react'
-import './App.css'
-import Nav from './components/Nav/Nav'
-import Header from './components/Header/Header'
-import Techstack from './components/TechStack/Techstack'
-import About from './components/About/About'
-import Projects from './components/Projects/Projects'
-import Contact from './components/Contact/Contact'
-import Footer from './components/Footer/Footer'
+import "./App.css";
 
 function App() {
-  return (
-    <div className='font-raleway'>
-      <Nav />
-      <Header />
-      <Techstack />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
-  )    
+	return <p>Portfolio</p>;
 }
 
-export default App
+export default App;
