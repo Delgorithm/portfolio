07455e432035b0d95e06b1807cfc84ce5c19383a
Delgorithm <delgorithm@gmail.com>
2024-05-19 12:43:31 +0200

    fixed the responsive for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,17 +11,17 @@ function Hero({ heroText }) {
 			</h1>
 			<article className="leading-6 font-light xxs:pt-2 xsl:pt-12 xxs:text-xs xs:text-base xsl:text-lg xs:pt-5 iphone12:flex iphone12:flex-col iphone12:gap-5">
 				<p className="xxs:text-xl xxs:mt-1">{heroText[0].firstP}</p>
-				<p className="mr-14 xxs:pt-3 xs:pt-3 xsl:pt-6 iphone12:text-base">
+				<p className="mr-14 xxs:pt-3 xs:pt-0 xsl:pt-6 iphone12:text-base">
 					{heroText[0].secondP}
 				</p>
-				<p className="mr-8 xxs:pt-4 xs:pt-3 xsl:pt-6 iphone12:text-base">
+				<p className="mr-8 xxs:pt-4 xs:pt-0 xsl:pt-6 iphone12:text-base">
 					{heroText[0].thirdP}
 				</p>
-				<p className="mr-8 xxs:pt-4 xs:pt-3 xsl:pt-6 iphone12:text-base">
+				<p className="mr-8 xxs:pt-4 xs:pt-0 xsl:pt-6 iphone12:text-base">
 					{heroText[0].fourthP}
 				</p>
 			</article>
-			<div className=" flex items-center justify-center xs:mt-10 iphone12:mt-5 motion-safe:animate-pulse font-thin">
+			<div className=" flex items-center justify-center xs:mt-10 iphone12:mt-0 motion-safe:animate-pulse font-thin">
 				<IoIosArrowRoundDown className="text-5xl pt-1" />
 			</div>
 		</section>
