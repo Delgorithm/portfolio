bf0f38a4176426ff77a2716f97371c316bf965fc
Delgorithm <delgorithm@gmail.com>
2024-05-29 13:56:32 +0200

    Fix : Added responsive on resume part

--- src/components/BtnResume.jsx
+++ src/components/BtnResume.jsx
@@ -19,8 +19,8 @@ function BtnResume() {
 			{!isOpen ? (
 				<>
 					<section className="fixed top-0 bg-mineshaft/95 h-full w-full">
-						<section className="relative top-[15%] h-full p-5">
-							<section className="flex flex-col justify-around items-center gap-5">
+						<section className="relative xxs:top-[15%] xs:top-[25%] iphone12:top-[25%] xl:top-[20%] iphone12pro:top-[25%] h-full p-5">
+							<section className="flex flex-col justify-around items-center gap-5 ">
 								<SlClose
 									onClick={handleOpen}
 									className="text-5xl text-silver fixed top-[15%] right-5"
@@ -28,22 +28,24 @@ function BtnResume() {
 								<img
 									src="/Img/cv.png"
 									alt="CV d'alternance d'Adrien Douville"
-									className="w-40"
+									className="xxs:w-40 xs:w-52 xsl:w-56 iphone12:w-52 iphone12pro:w-60 md:w-72"
 								/>
 								<div className="text-silver flex justify-around w-full items-center">
 									<a
 										href="/Img/cv_adrien_douville_cdi.pdf"
 										download="Adrien_Douville_cv_cdi.pdf"
 										className="flex flex-col items-center gap-4">
-										<p className="text-2xl">CDI</p>
-										<TbCloudDownload className="text-6xl text-tundora bg-pampas rounded-lg p-1 active:opacity-60 cursor-pointer" />
+										<p className="text-2xl xs:text-3xl  md:text-4xl">CDI</p>
+										<TbCloudDownload className="text-6xl xs:text-7xl md:text-8xl text-tundora bg-pampas rounded-lg p-1 active:opacity-60 cursor-pointer" />
 									</a>
 									<a
 										href="/Img/cv_adrien_douville_alternance.pdf"
 										download="Adrien_Douville_cv_alternance.pdf"
 										className="flex flex-col items-center gap-4">
-										<p className="text-2xl">Alternance</p>
-										<TbCloudDownload className="text-6xl text-tundora bg-pampas rounded-lg p-1 active:opacity-60 cursor-pointer" />
+										<p className="text-2xl  xs:text-3xl md:text-4xl">
+											Alternance
+										</p>
+										<TbCloudDownload className="text-6xl xs:text-7xl md:text-8xl text-tundora bg-pampas rounded-lg p-1 active:opacity-60 cursor-pointer" />
 									</a>
 								</div>
 							</section>
